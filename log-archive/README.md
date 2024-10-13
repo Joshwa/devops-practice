f5b78550a9e46fd68927a8e7ce38a638c92def35
josh <josh.t.reed@protonmail.com>
2024-10-13 15:35:50 -0500

    new file:   log-archive/README.md 	new file:   log-archive/log-archive.sh

---
+++ log-archive/README.md
@@ -0,0 +1,4 @@
+# Log Archive Tool
+----
+
+[project description](https://roadmap.sh/projects/log-archive-tool)
---
+++ log-archive/log-archive.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+log_dir=~/.log_archive
+
+if [ ! -d "$log_dir" ]; then
+    mkdir $log_dir
+fi
+
+tar -cvf $log_dir/logs_archive_$(date '+%Y%m%d_%H%M%S').tar.gz $1
